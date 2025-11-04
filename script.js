16ff80afa4fe504a4dd775d772fa2ed35f0e9604
Disha chaudhary <disha.chaudhary_cs24@gla.ac.in>
2025-11-03 23:35:26 -1200

    digital clock html css

---
+++ script.js
@@ -0,0 +1,39 @@
+let a=new Date()
+let h= a.getHours()
+let m = a.getMinutes()
+let s = a.getSeconds()
+let d = a.getDate()
+console.log(a)
+
+console.log(h,m,s,d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
